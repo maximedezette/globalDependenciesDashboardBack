e7a6ad789ff37f20869847d0ffceb53781bc1d2b
maxime.dezette <maxime.dezette@groupepsih.com>
2023-06-04 18:28:09 +0200

    ci(CodeCoverage): Adding generated annotation to exclude some code from coverage

--- src/main/java/com/globaldashboard/GlobalDependenciesDashboardApplication.java
+++ src/main/java/com/globaldashboard/GlobalDependenciesDashboardApplication.java
@@ -1,8 +1,10 @@
 package com.globaldashboard;
 
+import com.globaldashboard.common.domain.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Generated
 @SpringBootApplication
 public class GlobalDependenciesDashboardApplication {
 
---
+++ src/main/java/com/globaldashboard/common/domain/Generated.java
@@ -0,0 +1,15 @@
+package com.globaldashboard.common.domain;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, CONSTRUCTOR})
+public @interface Generated {
+
+}
