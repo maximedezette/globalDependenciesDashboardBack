205dfc74fe8cbf2e623ba584b58234a77635cdfe
maxime.dezette <maxime.dezette@groupepsih.com>
2023-06-21 19:10:31 +0200

    fix: Adding missing fields to RestProject :bug:

--- src/test/java/com/globaldashboard/dependencies/infrastructure/primary/RestProjectTest.java
+++ src/test/java/com/globaldashboard/dependencies/infrastructure/primary/RestProjectTest.java
@@ -4,8 +4,6 @@ import com.globaldashboard.dependencies.domain.Dependency;
 import com.globaldashboard.dependencies.domain.GroupId;
 import com.globaldashboard.dependencies.domain.Project;
 import com.globaldashboard.dependencies.domain.SemanticVersion;
-import com.globaldashboard.dependencies.infrastructure.primary.RestDependency;
-import com.globaldashboard.dependencies.infrastructure.primary.RestProject;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,15 +15,16 @@ class RestProjectTest {
 
     @Test
     void shouldConvertFromDomainProject() {
-        Project project = new Project(SemanticVersion.from("1.2.3-SNAPSHOT"), "name", "description", "17", getDependencies(), "");
+        Project project = new Project(SemanticVersion.from("1.2.3-SNAPSHOT"), "name", "description", "17", getDependencies(), "https://raw.githubusercontent.com/maximedezette/globalDependenciesDashboardBack/main/pom.xml");
 
         RestProject restProject = RestProject.from(project);
 
         assertThat(restProject).isNotNull();
         assertThat(restProject.version()).isEqualTo("1.2.3-SNAPSHOT");
-        assertThat(restProject.projectName()).isEqualTo("name");
+        assertThat(restProject.name()).isEqualTo("name");
         assertThat(restProject.description()).isEqualTo("description");
         assertThat(restProject.java()).isEqualTo("17");
+        assertThat(restProject.pomURL()).isEqualTo("https://raw.githubusercontent.com/maximedezette/globalDependenciesDashboardBack/main/pom.xml");
         assertThat(restProject.dependencies()).containsExactlyElementsOf(getRestDependencies());
     }
 
