a0f3c3c0c7c6ad5fce664bd2c2476e92d1807a73
maxime.dezette <maxime.dezette@groupepsih.com>
2023-07-01 16:42:56 +0200

    test: Adding 2 missing tests

--- src/test/java/com/globaldashboard/unit/dependencies/infrastructure/primary/RestDependencyTest.java
+++ src/test/java/com/globaldashboard/unit/dependencies/infrastructure/primary/RestDependencyTest.java
@@ -1,8 +1,12 @@
 package com.globaldashboard.unit.dependencies.infrastructure.primary;
 
 import com.globaldashboard.dependencies.domain.Dependency;
+import com.globaldashboard.dependencies.domain.GroupId;
 import com.globaldashboard.dependencies.infrastructure.primary.RestDependency;
+import com.globaldashboard.domain.ArtifactId;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +16,16 @@ class RestDependencyTest {
     void shouldBeBuildableFromDomain() {
         Dependency dependency = new Dependency("groupId", "artifact-id", "1.2.3-SNAPSHOT");
         RestDependency expectedRestDependency = new RestDependency("groupId", "artifact-id", "1.2.3-SNAPSHOT");
+
+        RestDependency restDependency = RestDependency.from(dependency);
+
+        assertThat(restDependency).isEqualTo(expectedRestDependency);
+    }
+
+    @Test
+    void shouldBeBuildableFromDomainWithNoVersion() {
+        Dependency dependency = new Dependency(new GroupId("groupId"), new ArtifactId("artifact-id"), Optional.empty());
+        RestDependency expectedRestDependency = new RestDependency("groupId", "artifact-id", "");
 
         RestDependency restDependency = RestDependency.from(dependency);
 
