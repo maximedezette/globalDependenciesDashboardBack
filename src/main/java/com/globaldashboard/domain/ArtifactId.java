d57fcb7370ca6011cd3f1e671661f83da1fec1bf
maxime.dezette <maxime.dezette@groupepsih.com>
2023-07-01 16:23:13 +0200

    refactor: Adding Guava's check to some value object creation (ArtifactId and GroupId)

--- src/main/java/com/globaldashboard/domain/ArtifactId.java
+++ src/main/java/com/globaldashboard/domain/ArtifactId.java
@@ -1,11 +1,16 @@
 package com.globaldashboard.domain;
 
 
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.regex.Pattern;
 
 public record ArtifactId(String name) {
 
     public ArtifactId {
+        Preconditions.checkArgument(StringUtils.isNotBlank(name), "Can't build an artifactId with no value");
+
         boolean isInvalid = Pattern.compile(".*[A-Z].*")
                 .matcher(name)
                 .matches();
--- src/test/java/com/globaldashboard/unit/dependencies/domain/ArtifactIdTest.java
+++ src/test/java/com/globaldashboard/unit/dependencies/domain/ArtifactIdTest.java
@@ -13,6 +13,18 @@ public class ArtifactIdTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new ArtifactId("LOG4J"));
     }
+
+    @Test
+    void shouldNotBuildArtifactIfWithNoName() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new ArtifactId(""));
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new ArtifactId(null));
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new ArtifactId("    "));
+    }
     @Test
     void shouldBuildValidArtifactId() {
         ArtifactId artifactId = new ArtifactId("artifact-id");
