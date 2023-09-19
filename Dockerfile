bee25437fdc3a2a52efaef57c5035490596d7cc4
maxime.dezette <m.dezette@gmail.com>
2023-09-19 18:10:14 +0200

    chore: Adding PostgreSQL database in docker container

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:18-jdk-alpine
-ARG WAR_FILE=target/*.war
-COPY ${WAR_FILE} app.war
-ENTRYPOINT ["java","-jar","/app.war"]
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO PROJECT (id, name, pom_url) VALUES (1, 'AperoTech', 'https://raw.githubusercontent.com/maximedezette/globalDependenciesDashboardBack/main/pom.xml');
-INSERT INTO PROJECT (id, name, pom_url) VALUES (2, 'KataApi', 'https://raw.githubusercontent.com/maximedezette/kata-api/main/pom.xml');
